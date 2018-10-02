dffe7c2969e3eccb75760c7076c8504443ba2286
Tri Lam <letrilam56@gmail.com>
2018-10-02 20:28:22 +0800

    Finish working and refactored palindrome method

--- lib/tlam_palindrome.rb
+++ lib/tlam_palindrome.rb
@@ -11,10 +11,23 @@ class String
     processed_content == processed_content.reverse
   end
 
+  # Returns the letters in the string.
+  # def letters
+    # self.chars.select { |c| c.match(/[a-z]/i) }.join
+    # the_letters = []
+    # letter_regex = /[a-z]/i
+    # self.chars.each do |character|
+    #   if character.match(letter_regex)
+    #     the_letters << character
+    #   end
+    # end
+    # the_letters.join
+  # end
+
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.downcase
+      self.scan(/[a-z]/i).join.downcase
     end
 end
--- test/tlam_palindrome_test.rb
+++ test/tlam_palindrome_test.rb
@@ -13,4 +13,16 @@ class TlamPalindromeTest < Minitest::Test
     assert "racecar".palindrome?
   end
 
+  def test_mixed_case_palindrome
+    assert "rAcEcar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  # def test_letters
+    # assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
+  # end
+
 end
